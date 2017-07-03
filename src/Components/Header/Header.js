e6811773127f0e4a9c9f3e122bb62ec124decdf1
kyozone <judolew@gmail.com>
2017-07-03 23:39:33 +0800

    Add App and Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import './Header.css';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="row header">
+        <div className="col-sm-2">
+        </div>
+        <div className="col-sm-8">
+          <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Search" />
+        </div>
+        <div className="col-sm-1">
+          <a href="#" id="signup">Signup</a>
+        </div>
+        <div className="col-sm-1">
+          <a href="#">Login</a>
+        </div>
+      </div>
+
+    );
+  }
+}
+export default Header;
