1850cf95db6e40efb41d604e37da66c8d18781a4
rubyflames <rubyflames@users.noreply.github.com>
2017-07-06 08:00:49 +0800

    attendees component added

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,10 +1,7 @@
-import React, {PropTypes} from 'react';
+import React, {Component} from 'react';
 
-export default class MyComponent extends React.Component {
+export class UserProfile extends Components {
   render() {
     return (<div>MyComponent</div>);
   }
 }
-
-.propTypes = {
-};
