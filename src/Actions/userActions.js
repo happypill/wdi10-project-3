c5183e3bd9a4e95f6a11bb691a02c7aa3cb11952
kyozone <judolew@gmail.com>
2017-07-08 16:04:30 +0800

    add User Action

--- src/Actions/userActions.js
+++ src/Actions/userActions.js
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+
+
+export const JOIN_EVENT=(event)=>{
+   return{
+      type:"FETCH_EVENTS",
+      event
+   }
+}
+export const REMOVE_EVENT=(event)=>{
+   return{
+      type:"FETCH_VENUE",
+      event
+   }
+}
+
+
